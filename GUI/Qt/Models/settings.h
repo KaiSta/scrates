e18d449d43fd038799a8404a4817deed28597f10
Jochen Klein <klein.jochen@gmail.com>
2015-04-20 23:56:12 +0200

    ADDED: static members for settings

--- GUI/Qt/Models/settings.h
+++ GUI/Qt/Models/settings.h
@@ -1,21 +1,23 @@
 #pragma once
 
+
+#include <QtGui>
 #include <QObject>
 #include <QSettings>
+
 
 class Settings : public QSettings
 {
     Q_OBJECT
 public:
-    // static Settings& instance();
     Settings(QObject* parent = 0);
     Settings(Format format, Scope scope, const QString& organization, const QString& application = QString(), QObject * parent = 0);
     ~Settings();
 
     Q_INVOKABLE void setValue(const QString& key, const QVariant& value);
     Q_INVOKABLE QVariant value(const QString& key, const QVariant& defaultValue = QVariant()) const;
-
-public slots:
+    static std::string containerLocation();
+    static std::string vhdLocation();
 signals:
   void settingChanged(const QString& key);
 };
