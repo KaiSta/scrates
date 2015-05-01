5a926f0448c60db8d0c36e42b38d46c64c32d2dd
Jochen Klein <klein.jochen@gmail.com>
2015-05-02 00:25:41 +0200

    location strings and callback function

--- GUI/Qt/Models/container.cpp
+++ GUI/Qt/Models/container.cpp
@@ -4,8 +4,8 @@ ContainerObject::ContainerObject(QObject* parent)
     : QObject(parent), controller_([this](container_event e) { callbackFunc(e); }, Settings::vhdLocation())
 { }
 
-ContainerObject::ContainerObject(const QString& name, const QString& syncLocation, bool isOpen, QObject* parent)
-    : name_(name), syncLocation_(syncLocation), isOpen_(isOpen), QObject(parent),
+ContainerObject::ContainerObject(const QString& name, const QString& syncLocation, QObject* parent)
+    : name_(name), syncLocation_(syncLocation), QObject(parent), history_(QString()),
       controller_([this](container_event e) { callbackFunc(e); }, Settings::vhdLocation())
 { }
 
@@ -14,15 +14,18 @@ ContainerObject::~ContainerObject()
 
 QString ContainerObject::name() const
 {
-    return name_;
+    QString displayedName(name_);
+    if (isOpen()) displayedName.append("*");
+    return displayedName;
 }
+
 
 QString ContainerObject::path() const
 {
     return path_;
 }
 
-bool ContainerObject::isOpen()
+bool ContainerObject::isOpen() const
 {
     return controller_.is_open();
 }
@@ -41,12 +44,12 @@ void ContainerObject::setHistory(const QString& entry)
     emit historyChanged();
 }
 
-void ContainerObject::create(const QString& password, local_file::storage_type storeType, size_t storeSize)
+void ContainerObject::create(const QString& password, bool mount, local_file::storage_type storeType, size_t storeSize)
 {
-    controller_.create(name_.toStdString(), Settings::containerLocation(), password.toStdString(), "/Users/jochen/Desktop/tempest/cloud/", local_file::storage_type::FOLDER, 0);
+    controller_.create(name_.toStdString(), Settings::containerLocation(), password.toStdString(), this->syncLocation_.toStdString(), storeType, storeSize);
 
     // If the user doesnt want to automount the container after creating, demount it.
-    if (!this->isOpen() && controller_.is_open())
+    if (!mount && controller_.is_open())
         controller_.close();
 
     emit openChanged();
@@ -72,11 +75,12 @@ bool ContainerObject::exportHistory(const QString& url)
 
 bool ContainerObject::mount(const QString& password)
 {
-    std::string containerFullPath("/Users/jochen/Desktop/tempest/local/" + this->name_.toStdString() + ".cco");
+    std::string containerFullPath(Settings::containerLocation() + this->name_.toStdString() + ".cco");
 
     if (controller_.open(containerFullPath, password.toStdString(), local_file::storage_type::FOLDER))
     {
         emit openChanged();
+        emit nameChanged();
         return true;
     }
     return false;
@@ -86,6 +90,7 @@ void ContainerObject::unmount()
 {
     controller_.close();
     emit openChanged();
+    emit nameChanged();
 }
 
 bool ContainerObject::sync()
@@ -95,13 +100,85 @@ bool ContainerObject::sync()
 
 void ContainerObject::openDirectory(const QString& url)
 {
-    if (url.length())
+    if  (url.length())
         QDesktopServices::openUrl(QUrl(url, QUrl::TolerantMode));
     else
-        QDesktopServices::openUrl(QUrl(path_, QUrl::TolerantMode));
+        QDesktopServices::openUrl(QUrl("file://" + QString::fromStdString(Settings::vhdLocation()) + name_, QUrl::TolerantMode));
 }
 
 void ContainerObject::callbackFunc(container_event e)
 {
-    // TODO
+    // TODO: VERBOSE = debug infos. also am besten makro rein, dass nur bei debug build das ausgegeben wird
+    std::string type;
+    std::string message;
+    switch (e.type)
+    {
+    case INFORMATION:
+        type = "information";
+        break;
+    case CONFLICT:
+        type = "error";
+        break;
+    case WARNING:
+        type = "warning";
+        break;
+    case VERBOSE:
+        type = "verbose";
+        break;
+    }
+    switch (e.message)
+    {
+    case NONE:
+        message = "none";
+        break;
+    case CTR_FILE_NOT_FOUND:
+        message = "container file not found";
+        break;
+    case WRONG_PASSWORD:
+        message = "wrong password";
+        break;
+    case MISSING_ENC_FILES:
+        message = "missing encrypted file in cloud";
+        break;
+    case DECRYPTION_ERROR:
+        message = "decryption error";
+        break;
+    case WRONG_ARGUMENTS:
+        message = "wrong arguments";
+        break;
+    case SUCC:
+        message = "succ";
+        break;
+    case SYNCHRONIZING:
+        message = "sync";
+        break;
+    case FINISHED_SYNCHRONIZING:
+        message = "finished sync";
+        break;
+    case NO_WARNING:
+        message = "no warning";
+        break;
+    case CLOSING:
+        message = "closing container";
+        break;
+    case ADD_FILE:
+        message = "adding file " + std::string(e._data_.data());
+        break;
+    case UPDATE_FILE:
+        message = "updating file " + std::string(e._data_.data());
+        break;
+    case DELETE_FILE:
+        message = "delete file " + std::string(e._data_.data());
+        break;
+    case EXTRACT_FILE:
+        message = "extract file " + std::string(e._data_.data());
+        break;
+    case EXTRACT_FILES:
+        message = "extract files ";
+        break;
+    case INIT_CONTAINER:
+        message = "initializing container";
+        break;
+    }
+    setHistory(QString::fromStdString(type) + ": " + QString::fromStdString(message) + "<br/>");
 }
